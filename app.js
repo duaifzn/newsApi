d6b6dcdae7a7207b4e4484d4de5a73b403890bc1
duaifzn <duaifzn@gmail.com>
2021-05-31 14:47:10 +0800

    feat: init

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.set('view engine', 'ejs'); 
+
+app.get('/', (req, res) =>{
+    res.render('index')
+})
+
+app.listen(port, () =>{
+    console.log(`server running on port ${port}`)
+})
