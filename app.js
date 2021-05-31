3c2463d5882a7fcaf0fdcf3f44c8cc754d618052
onetail <paosong91@gmail.com>
2021-05-31 15:29:54 +0800

    chore: add moment js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) =>{
-    res.render('index')
-})
+app.use(express.static('views'));
 
-app.listen(port, () =>{
-    console.log(`server running on port ${port}`)
-})
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
